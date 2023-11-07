166c03619b887d4bc5b2a22633a093594749b77a
CrisJason7 <phongnh2@smartosc.com>
2023-11-07 09:55:34 +0700

    [Profile][UI]: Background image and left content

---
+++ src/presentation/features/profile/styles.ts
@@ -0,0 +1,134 @@
+import styled from '@emotion/styled';
+import COLOR from 'src/presentation/theme/Color';
+
+const Container = styled('div')({
+  padding: '30px 30px 100px',
+
+  '.profile__content-container': {
+    display: 'flex',
+    padding: '70px 0 0 50px',
+    width: '100%',
+    justifyContent: 'space-between',
+  },
+
+  '.flex-center': {
+    display: 'flex',
+    alignItems: 'center',
+  },
+});
+
+const BackgroundProfileImg = styled('div')({
+  width: '100%',
+  height: 300,
+  borderRadius: 20,
+  position: 'relative',
+  cursor: 'pointer',
+
+  img: {
+    width: '100%',
+    height: '100%',
+  },
+
+  '.profile__avatar-container': {
+    position: 'absolute',
+    top: 205,
+    left: 50,
+
+    img: {
+      width: 140,
+      height: 140,
+      borderRadius: '50%',
+      border: `4px solid ${COLOR.neutral.neutral_5}`,
+    },
+  },
+});
+
+const LeftContent = styled('div')({
+  width: '25%',
+
+  '.user-information-container': {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 24,
+
+    '.user-information__content': {
+      display: 'flex',
+      flexDirection: 'column',
+      gap: 10,
+
+      '.user-information__name': {
+        fontFamily: 'Bold',
+        fontSize: 24,
+        lineHeight: '32px',
+        color: COLOR.neutral.neutral_1,
+      },
+
+      '.user-information__content-container': {
+        gap: 8,
+
+        '.user-information__content-title': {
+          fontFamily: 'Bold',
+          fontSize: 14,
+          color: COLOR.neutral.neutral_4,
+          lineHeight: '18px',
+
+          span: {
+            fontFamily: 'Semibold',
+            color: COLOR.neutral.neutral_1,
+          },
+        },
+      },
+    },
+
+    '.user-information__description': {
+      fontFamily: 'Gilroy',
+      fontSize: 14,
+      lineHeight: '24px',
+      color: COLOR.neutral.neutral_4,
+    },
+
+    '.user-information__metrics-container': {
+      padding: 16,
+      justifyContent: 'space-between',
+      borderTop: `1px solid ${COLOR.neutral.neutral_3}`,
+      borderBottom: `1px solid ${COLOR.neutral.neutral_3}`,
+
+      '.user-information__metrics-item': {
+        flexDirection: 'column',
+        gap: 5,
+
+        '.user-information__metrics-value': {
+          fontFamily: 'Bold',
+          fontSize: 18,
+          lineHeight: '24px',
+          color: COLOR.neutral.neutral_1,
+        },
+
+        '.user-information__metrics-title': {
+          fontFamily: 'Semibold',
+          fontSize: 14,
+          lineHeight: '18px',
+          color: COLOR.neutral.neutral_2,
+        },
+      },
+    },
+  },
+
+  '.user-information__button': {
+    marginTop: 30,
+    width: '100%',
+
+    button: {
+      width: '100%',
+      padding: '11px 24px',
+      fontSize: 16,
+      lineHeight: '20px',
+    },
+  },
+});
+
+const RightContent = styled('div')({
+  width: '70%',
+});
+
+export { Container, BackgroundProfileImg, LeftContent, RightContent };
