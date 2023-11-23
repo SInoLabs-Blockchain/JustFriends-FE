1a5341e45a5916a36cdad365c155987b873f0dc9
linhdh1 <linhdh1@smartosc.com>
2023-11-23 15:00:36 +0700

    fix condition

--- src/data/config/apolloClient.ts
+++ src/data/config/apolloClient.ts
@@ -1,6 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const apolloClient = new ApolloClient({
-  uri: process.env.REACT_APP_THE_GRAPH_URL,
+  // uri: process.env.REACT_APP_THE_GRAPH_URL,
+  uri: "http://192.168.10.134:8000/subgraphs/name/example",
   cache: new InMemoryCache(),
 });
