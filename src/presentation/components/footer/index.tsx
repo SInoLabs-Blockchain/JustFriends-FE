d161209353802823f0cfe151e2156cbc2981a891
dunglv-smartosc <dunglv@smartosc.com>
2023-11-27 19:39:27 +0700

    Hotfix: Update footer

--- src/presentation/components/footer/index.tsx
+++ src/presentation/components/footer/index.tsx
@@ -1,58 +1,61 @@
-import { Box } from '@mui/material';
+import { Box, Typography } from "@mui/material";
 import {
   FacebookIcon,
   LinkedInIcon,
-  Logo,
   MediumIcon,
   TwitterIcon,
   YoutubeIcon,
-} from 'src/presentation/theme/assets/icons';
+} from "src/presentation/theme/assets/icons";
 import {
   AddressText,
   FooterContainer,
   SocialContainer,
   SocialSection,
-} from './styles';
+} from "./styles";
+import Logo from "src/presentation/theme/assets/images/JustFriends.png";
 
 const Footer = () => {
   return (
     <FooterContainer>
-      <Logo />
-      <Box className='divider' />
+      <Box className="footer__logo">
+        <img src={Logo} alt="logo" />
+        <Typography>JustFriends</Typography>
+      </Box>
+      <Box className="divider" />
       <SocialContainer>
         <SocialSection>
           <a
-            href='https://www.facebook.com/sinolabs'
-            target='_blank'
-            rel='noreferrer'
+            href="https://www.facebook.com/sinolabs"
+            target="_blank"
+            rel="noreferrer"
           >
             <FacebookIcon />
           </a>
           <a
-            href='https://twitter.com/sino_labs'
-            target='_blank'
-            rel='noreferrer'
+            href="https://twitter.com/sino_labs"
+            target="_blank"
+            rel="noreferrer"
           >
             <TwitterIcon />
           </a>
           <a
-            href='https://www.linkedin.com/company/sino-labs/'
-            target='_blank'
-            rel='noreferrer'
+            href="https://www.linkedin.com/company/sino-labs/"
+            target="_blank"
+            rel="noreferrer"
           >
             <LinkedInIcon />
           </a>
           <a
-            href='https://www.youtube.com/@SInoLabsBlc'
-            target='_blank'
-            rel='noreferrer'
+            href="https://www.youtube.com/@SInoLabsBlc"
+            target="_blank"
+            rel="noreferrer"
           >
             <YoutubeIcon />
           </a>
           <a
-            href='https://www.youtube.com/@SInoLabsBlc'
-            target='_blank'
-            rel='noreferrer'
+            href="https://www.youtube.com/@SInoLabsBlc"
+            target="_blank"
+            rel="noreferrer"
           >
             <MediumIcon />
           </a>
--- src/presentation/components/footer/styles.ts
+++ src/presentation/components/footer/styles.ts
@@ -1,40 +1,54 @@
-import { Typography } from '@mui/material';
-import { styled } from '@mui/material/styles';
-import COLOR from 'src/presentation/theme/Color';
+import { Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import COLOR from "src/presentation/theme/Color";
 
-const FooterContainer = styled('div')({
-  width: '100%',
-  padding: '40px 30px',
+const FooterContainer = styled("div")({
+  width: "100%",
+  padding: "40px 30px",
   backgroundColor: COLOR.white,
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
   gap: 30,
 
-  '.divider': {
-    width: '100%',
+  ".footer__logo": {
+    display: "flex",
+    gap: "8px",
+    alignItems: "center",
+    p: {
+      fontSize: "36px",
+      fontFamily: "SemiBold",
+    },
+    img: {
+      width: "64px",
+      height: "64px",
+    },
+  },
+
+  ".divider": {
+    width: "100%",
     height: 1,
     backgroundColor: COLOR.border,
   },
 });
 
-const SocialContainer = styled('div')({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
+const SocialContainer = styled("div")({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
   gap: 24,
 });
 
-const SocialSection = styled('div')({
-  display: 'flex',
-  alignItems: 'center',
+const SocialSection = styled("div")({
+  display: "flex",
+  alignItems: "center",
   gap: 15,
 });
 
 const AddressText = styled(Typography)({
   fontSize: 14,
-  fontFamily: 'Regular',
-  lineHeight: '18px',
+  fontFamily: "Regular",
+  lineHeight: "18px",
   color: COLOR.neutral.neutral_4,
 });
 
