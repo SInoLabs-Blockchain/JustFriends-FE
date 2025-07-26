6fa12a2d582c1f2a7a63ca34584cf870b42697c9
nhatcl-smartosc <nhatcl@smartosc.com>
2025-07-26 13:57:45 +0700

    update pregas

--- src/common/utils/wallet.ts
+++ src/common/utils/wallet.ts
@@ -42,7 +42,7 @@ export const DefaultsForUserOp: UserOperation = {
   callData: "0x",
   callGasLimit: 0,
   verificationGasLimit: 150000, // default verification gas. will add create2 cost (3200+200*length) if initCode exists
-  preVerificationGas: 50000, // should also cover calldata cost.
+  preVerificationGas: 60000, // should also cover calldata cost.
   maxFeePerGas: 0,
   maxPriorityFeePerGas: 1e9,
   paymasterAndData: "0x",
